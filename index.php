82f11b592fef3619ff543831c72e18a7a1c61bbc
Carmine Lentisco <carminelentisco@gmail.com>
2020-05-15 14:49:25 +0200

    INDEX.PHP
    aggiunto codice php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP first step</title>
+</head>
+<body>
+    
+    <!-- VAR -->
+
+    <?php 
+        $text_base = "Pippo mangia le mele dell' albero";
+        $censored = $_GET['censored'];
+        $censored_string = '***';
+        $replace = str_replace($censored, $censored_string, $text_base);
+    ?>
+
+    <h1>Esercizio</h1>
+    <p>Inserisci, censored="prola da censurare", per censurare la parola scelta </p>
+
+    <hr>
+
+    <p><strong>Frase originale: </strong><?php echo $text_base ?></p>
+    <p><strong>Frase censurata: </strong><?php echo $replace ?></p>
+
+    <hr>
+</body>
+</html>
